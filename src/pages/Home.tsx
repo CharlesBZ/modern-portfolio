cf3cf027970302e7acde42315c0c2aabff778bbc
CharlesBZ <charles.a.bestjr@gmail.com>
2025-06-25 08:31:52 -0400

    TailwindCSS Setup and Theme Customization

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+export const Home = () => {
+  return(
+    <div className="min-h-screen flex flex-col">
+      <h2 className="">Home</h2>
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+
+      {/* Footer */}
+    </div>
+  )
+}
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return(
+    <div>
+      Not here lolz
+    </div>
+  )
+}
